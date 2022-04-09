2ebf10125955918e58d27db472a35e41af0b3d9d
Davide Scarrà <73951384+LoScarro@users.noreply.github.com>
2022-04-09 15:39:55 +0200

    Add files via upload
    Funziona quasi tutto. Sistemare problema thread con risultato uguale

--- labo1.c
+++ labo1.c
@@ -3,13 +3,21 @@
 #include <time.h>
 #include <pthread.h>
 
+#define R1 4		//Numero di righe della matrice A
+#define C1 4		//Numero di colonne della matrice A
+#define R2 4		//Numero di righe della matrice B
+#define C2 4		//Numero di colonne della matrice B
+#define R3 4		//Numero di righe della matrice C
+#define C3 4		//Numero di colonne della matrice C
+#define BLOCK 2		//Numero di blocchi in cui dividere le matrici
+
 pthread_barrier_t barrier;
 
 struct Matrix
 {
 	int rows;	// number of rows
 	int cols;	// number of columns
-	int **data; // <- note that this is a pointer to one dim array
+	float **data; // <- note that this is a pointer to one dim array
 };
 
 struct toMult
@@ -18,11 +26,11 @@ struct toMult
 	struct Matrix* mat2;
 };
 
-int **allocMatrix(struct Matrix mat)
+float **allocMatrix(struct Matrix mat)
 {
-	mat.data = malloc(mat.rows * sizeof(int *));
+	mat.data = malloc(mat.rows * sizeof(float *));
 	for (int i = 0; i < mat.rows; i++)
-		mat.data[i] = (int *)calloc(mat.cols, sizeof(int));
+		mat.data[i] = (float *)calloc(mat.cols, sizeof(float));
 
 	return mat.data;
 }
@@ -32,7 +40,7 @@ void print(struct Matrix mat)
 	for (int i = 0; i < mat.rows; i++)
 	{
 		for (int j = 0; j < mat.cols; j++)
-			printf("%d ", mat.data[i][j]);
+			printf("%f ", mat.data[i][j]);
 		printf("\n");
 	}
 }
@@ -63,59 +71,47 @@ void *multiply(void *var)
 	return (void*) result;
 }
 
-int **init(struct Matrix mat)
+float **init(struct Matrix mat)
 {
-	srand(time(NULL)); // Initialization, should only be called once.
+	srand(time(NULL));
 
 	int i, j, count;
 
-	// Note that arr[i][j] is same as *(*(arr+i)+j)
 	count = 0;
 	for (i = 0; i < mat.rows; i++)
 		for (j = 0; j < mat.cols; j++)
-			mat.data[i][j] = (rand() % 10); // Or *(*(arr+i)+j) = ++count
-
-	// print(mat);
+			mat.data[i][j] = ((float)rand()/RAND_MAX)*(float)(10.0);
 
 	return mat.data;
 }
 
-int **merge(struct Matrix mat1, struct Matrix mat2)
+struct Matrix merge(struct Matrix mat1, struct Matrix mat2)
 {
-
 	struct Matrix res = {mat1.rows + mat2.rows, mat2.cols};
 	res.data = allocMatrix(res);
 
-	for (int i = 0; i < mat1.rows; i++)
-	{
-		for (int j = 0; j < mat1.cols; j++)
-		{
+	for (int i = 0; i < mat1.rows; i++){
+		for (int j = 0; j < mat1.cols; j++){
 			res.data[i][j] = mat1.data[i][j];
 		}
 	}
 
-	for (int i = mat1.rows; i < res.rows; i++)
-	{
-		for (int j = 0; j < res.cols; j++)
-		{
+	for (int i = mat1.rows; i < res.rows; i++){
+		for (int j = 0; j < res.cols; j++){
 			res.data[i][j] = mat2.data[i - mat1.rows][j];
 		}
 	}
-
-	return res.data;
+	return res;
 }
 
-struct Matrix decomp(int row, struct Matrix mat1, int index)
-{
+struct Matrix decomp(int row, struct Matrix mat1, int index){
 	struct Matrix temp = {row, mat1.cols};
 	temp.data = allocMatrix(temp);
 
 	int l = 0;
 
-	for (int i = index; i < index+row; i++)
-	{
-		for (int j = 0; j < mat1.cols; j++)
-		{
+	for (int i = index; i < index+row; i++){
+		for (int j = 0; j < mat1.cols; j++){
 			temp.data[l][j] = mat1.data[i][j];
 		}
 		l++;
@@ -124,7 +120,7 @@ struct Matrix decomp(int row, struct Matrix mat1, int index)
 	return temp;
 }
 
-void threadCreate(struct Matrix *mat1, struct Matrix *mat2, int block)
+struct Matrix threadCreate(struct Matrix *mat1, struct Matrix *mat2, int block)
 {
 	pthread_barrier_init(&barrier, NULL, block);
 	int row = mat1->rows / block;
@@ -152,24 +148,27 @@ void threadCreate(struct Matrix *mat1, struct Matrix *mat2, int block)
 		
 		//-----------------------------------------------------------------------------------------------------------
 
-		for (count = 0; count < block; ++count)
+		for (count = 0; count < block; count++)
 		{
 			temp=decomp(row, *mat1, index);
+			index += row;
 			args.mat1 = &temp;
+			
 			if (pthread_create(&threads[count], NULL, &multiply, &args) != 0)
 			{
 				fprintf(stderr, "error: Cannot create thread # %d\n", count);
 				break;
 			}
-			index += row;
+			
 		}
 		for (int i = 0; i < count; ++i)
 		{
-			if (pthread_join(threads[i], &retvals[i]) != 0)
+			if (pthread_join(threads[i], (void*) &retvals[i]) != 0)
 			{
 				fprintf(stderr, "error: Cannot join thread # %d\n", i);
 			}
-			print(*retvals[i]);
+			//print(*retvals[i]);
+			res=merge(res, *retvals[i]);
 		}
 		//-------------------------------------------------------------------------------------------------------------
 
@@ -180,23 +179,26 @@ void threadCreate(struct Matrix *mat1, struct Matrix *mat2, int block)
 		//printf("%d", retvals[j].rows);
 	
 	pthread_barrier_destroy(&barrier);
+	return(res);
 	printf("----------------\n");
 }
 
 int main()
 {
+	clock_t start = clock();
 	// int **mat1, **mat2, **mat3, **res;
-	int row1, col1, row2, col2, row3, col3, blockNumber, blockSize;
+	int row1, col1, row2, col2, row3, col3;
 
 	/*printf("Quante righe ha la prima matrice? ");
 	scanf("%d", &row1);
 	printf("Quante colonne ha la prima matrice? ");
 	scanf("%d", &col1);*/
 
-	struct Matrix mat1 = {4, 4};
+	struct Matrix mat1 = {R1, C1};
 	mat1.data = allocMatrix(mat1);
 	mat1.data = init(mat1);
 
+	printf("La matrice A è:\n");
 	print(mat1);
 
 	/*printf("Quante righe ha la seconda matrice? ");
@@ -204,13 +206,32 @@ int main()
 	printf("Quante colonne ha la seconda matrice? ");
 	scanf("%d", &col2);*/
 
-	struct Matrix mat2 = {4, 4};
+	struct Matrix mat2 = {R2, C2};
 	mat2.data = allocMatrix(mat2);
 	mat2.data = init(mat2);
 
+	printf("La matrice B è:\n");
 	print(mat2);
-	printf("-------------------------\n");
-	threadCreate(&mat1, &mat2, 2);
+	
+	struct Matrix mat3 = {R3, C3};
+	mat3.data = allocMatrix(mat3);
+	mat3.data = init(mat3);
+
+	printf("La matrice C è:\n");
+	print(mat3);
+
+	struct Matrix res1 = {R1, C2};
+	res1.data = allocMatrix(res1);
+
+	res1=threadCreate(&mat1, &mat2, BLOCK);
+	printf("\n");
+	printf("Il risultato del calcolo è:\n");
+	print(threadCreate(&mat3, &res1 , BLOCK));
+	printf("\n");
+
+	clock_t end = clock();
+
+	printf("Tempo di esecuzione =  %f secondi \n", ((double)(end - start)) / CLOCKS_PER_SEC);
 	// multiply(mat1, mat2);
 
 	/*struct Matrix RES = {A.rows, B.cols, res};
